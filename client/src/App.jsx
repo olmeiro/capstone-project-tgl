52877b8500f3d4f353aba497cdf039bd33c73155
canonatalia <naty920714@gmail.com>
2022-07-18 18:35:52 -0500

    view home pagine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useSearchParams
+} from "react-router-dom";
+import logo from './images/logo.png';
+import logout from './images/logout.png';
+import Home from './components/home';
+import Profile from './components/profile';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="App-header">
+        <div id = 'headerApp'>
+
+          <BrowserRouter>
+            <nav className='header'>
+              <img src={logo} className="App-logo" alt="logo"/>
+              <h2 className='titule'>RED SOCIAL TGL</h2>
+              <Link to="home" className='optionPage'> Home </Link>
+              <Link to="profile" className='optionPage'> Profile </Link>
+              <h4 className='userName'>Welcome ${"User Name"}</h4>
+              <img src={logout} className="logout" alt="logout" />
+            </nav>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="home" element={<Home/>} />
+              <Route path="profile" element={<Profile/>} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/login/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './images/logo.png';
+
+
+export const index = () => {
+  return (
+    <div>
+      <div>
+        <img src={logo} className="logoMainPage"/>
+
+      </div>
+      <div>
+
+      </div>
+
+    </div>
+  )
+}
