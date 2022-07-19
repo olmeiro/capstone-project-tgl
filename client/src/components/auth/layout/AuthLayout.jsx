6c00218bf1b32776038cbabf591e0abe366074ad
Olmeiro orozco <olme.orozco@gmail.com>
2022-07-19 09:53:43 -0500

    init taiwind css

--- client/src/components/auth/layout/AuthLayout.jsx
+++ client/src/components/auth/layout/AuthLayout.jsx
@@ -2,17 +2,17 @@ import PropTypes from 'prop-types'
 
 export const AuthLayout = ({ children, title = '' }) => {
   return (
-    <>
-    <nav>
+    <div >
+    <div className='w-full border-solid h-64 bg-black text-cyan-200'>
       este es el nav
-    </nav>
+    </div>
       <h1>Social Media TGL</h1>
       <h2>{title}</h2>
       {children}
     <footer>
       <p>Este es el footer</p>
     </footer>
-    </>
+    </div>
   )
 }
 
