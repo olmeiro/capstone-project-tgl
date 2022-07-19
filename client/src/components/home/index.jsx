a538ca6b14cc7b16822f1598b5a3cfaf33945a47
canonatalia <naty920714@gmail.com>
2022-07-19 17:19:39 -0500

    return view with css

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -6,44 +6,8 @@ import postImage from '../../images/postImage.png';
 
 function Home() {
     return (
-        <div className='columns'>
-            <div className='suggestions'>
-                <div>
-                    <label>User Name</label>
-                    <img src={user} className="userSuggestionImage"/>
-                </div>
-                <div>
-                    <label>User Name</label>
-                    <img src={user} className="userSuggestionImage"/>
-                </div>
-                <div>
-                    <label>User Name</label>
-                    <img src={user} className="userSuggestionImage"/>
-                </div>
-            </div>
-
-            <div className='posts'>
-                <div>
-                    <img src={user} className="userPostImage"/>
-                    <label>User Name</label>
-                </div>
-                <div className="postImagenes">
-                    <img src={postImage}/>
-                    <p>description Image</p>
-                </div>
-            </div>
-
-            <div className='publicity'>
-                <div>
-                    <img src={publicity} className="publicityImage"/>
-                </div>
-                <div>
-                    <img src={publicity} className="publicityImage"/>
-                </div>
-                <div>
-                    <img src={publicity} className="publicityImage"/>
-                </div>
-            </div>
+        <div>
+            
         </div>
     );
 };
