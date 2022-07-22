38e2fbe8046c521697b5d8c8f2f5f2a52f81056e
Albert-25 <ajst_95@outlook.com>
2022-07-21 22:08:49 -0500

    axios eliminador

--- api/src/axios/axios.js
+++
@@ -1,5 +0,0 @@
-const axios = require("axios");
-const postImagen = async (imagen) => {
-    const imagenPosteada = await axios.post("https://api.imgbb.com/1/upload?key=32b788816a3666677b28fe86f73c91fb", imagen);
-}
-module.exports = { postImagen };
