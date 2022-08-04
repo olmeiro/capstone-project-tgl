c06fca540e32e94a49a324fc294db7182f797f95
Olmeiro orozco <olme.orozco@gmail.com>
2022-08-04 13:41:57 -0500

    configuracion jest y testing library front

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,2 +1,18 @@
 // En caso de necesitar la implementación del FetchAPI
-import 'whatwg-fetch' // <-- yarn add whatwg-fetch
+// yarn add -D whatwg-fetch
+// import 'whatwg-fetch';
+
+// En caso de encontrar paquetes que lo requieran
+// yarn add -D setimmediate
+// import 'setimmediate';
+
+// En caso de tener variables de entorno y aún no soporta el import.meta.env
+// yarn add -D dotenv
+require('dotenv').config({
+  path: '.env.test'
+})
+
+// Realizar el mock completo de las variables de entorno
+jest.mock('./src/helpers/getEnvVariables', () => ({
+  getEnvVariables: () => ({ ...process.env })
+}))
--- client/tests/components/home/layout/HomeLayout.test.js
+++
@@ -1,6 +0,0 @@
-describe('Test component <HomeLayout />', () => {
-  
-  test('Component should be match with snapshot', () => {
-      
-  })
-})
