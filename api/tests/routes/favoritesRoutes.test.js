cd2fcddd53027fed2339fa265d0d65e8d7fc22fa
Albert-25 <ajst_95@outlook.com>
2022-08-07 12:35:45 -0500

    test de favorites routes agregado

---
+++ api/tests/routes/favoritesRoutes.test.js
@@ -0,0 +1,53 @@
+const app = require("../../app")
+const request = require("supertest")
+
+describe("GET /favorites from a user", () => {
+
+    test("should response with a 200 status code", async () => {
+        const response = await request(app).get("/favorites/7").send()
+        expect(response.statusCode).toBe(200)
+    })
+
+    test("should response an Object", async () => {
+        const response = await request(app).get("/favorites/7").send()
+        expect(response.body).toBeInstanceOf(Object)
+    })
+
+    test("should response a content-type: application/json; charset=utf-8", async () => {
+        const response = await request(app).get("/favorites/7").send()
+        expect(response.headers["content-type"]).toEqual("application/json; charset=utf-8")
+    })
+
+})
+
+
+describe("POST / add a favorite",()=>{
+    test("should response with a 200 status code",async ()=>{
+        const response = await request(app).post("/favorites?postId=5&userId=1").send()
+        expect(response.statusCode).toBe(200)
+    } )
+
+    test("should response an Object", async () => {
+        const response = await request(app).post("/favorites?postId=5&userId=1").send()
+        expect(response.body).toBeInstanceOf(Object)
+    })
+
+    test("should response a content-type: application/json; charset=utf-8", async () => {
+        const response = await request(app).post("/favorites?postId=5&userId=1").send()
+        expect(response.headers["content-type"]).toEqual("application/json; charset=utf-8")
+    })
+
+})
+
+describe("DELETE / delete a favorite", ()=>{
+
+    test("should response with a 200 status code", async () => {
+        const response = await request(app).delete("/favorites?postId=1&userId=1").send()
+        expect(response.statusCode).toBe(200)
+    })
+
+    test("should response a content-type: application/json; charset=utf-8", async () => {
+        const response = await request(app).delete("/favorites?postId=1&userId=1").send()
+        expect(response.headers["content-type"]).toEqual("application/json; charset=utf-8")
+    })
+})
