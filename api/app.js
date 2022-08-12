2836ab4088bcaeb8e11a1c9bb4b24a267ba848d6
Albert-25 <ajst_95@outlook.com>
2022-08-11 22:57:17 -0500

    socket.io agregado a la carpeta app.js

--- api/app.js
+++ api/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const cors = require("cors");
+const http = require("http")
+const { Server } = require("socket.io")
 
 const router = require("./src/routes/index");
 const app = express()
 
+const serverHttp = http.createServer(app)
+const io = new Server(serverHttp)
 
 app.use(cors());
 app.use(express.json());
