a8b84456743575e8be63a27192dc59f38f3d808a
Albert-25 <ajst_95@outlook.com>
2022-08-11 23:25:52 -0500

    socket agregado al front y conexión hecha con el back

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store'
-
 import './styles/tailwind.css'
+import io from "socket.io-client"
+
+const socket = io("http://localhost:3000")
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
