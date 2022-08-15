e89adc6007f0b26ad948802b5f74b88c68adbd0e
Olmeiro orozco <olme.orozco@gmail.com>
2022-08-15 15:40:16 -0500

    pruebas en AppRouter

--- client/jest.config.cjs
+++ client/jest.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
   },
   // ModuleNameMapper sólo si ocupamos importar CSS en nuestros componentes para el testing
   moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/mocks/fileMock.js'
   }
 }
--- client/src/helpers/getEnvVariables.js
+++ client/src/helpers/getEnvVariables.js
@@ -3,6 +3,7 @@ export const getEnvVariables = () => {
 
   return {
     // ...import.meta.env
+    VITE_MODE: import.meta.env.VITE_MODE,
     VITE_URL_API: import.meta.env.VITE_URL_API,
     VITE_API_KEY: import.meta.env.VITE_API_KEY
   }
