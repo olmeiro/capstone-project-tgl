b3ed6502bf09eeb181ba45027aa3867a344649f8
Albert-25 <ajst_95@outlook.com>
2022-08-16 11:08:02 -0500

    sockets agregados al back

--- api/app.js
+++ api/app.js
@@ -8,14 +8,47 @@ const app = express()
 
 const serverHttp = http.createServer(app)
 const io = new Server(serverHttp, {
-    cors:{
-        origin:"http://localhost:5173"// url del frontend 
+    cors: {
+        origin: "http://localhost:5173"// url del frontend 
     }
 })
 
-io.on("connection",(socket)=>{
-    console.log("Sockect ID: ",socket.id)
-    console.log("Usuario conectado!!")
+const getUser = (userId) => {
+    return users.find(user => user.userId == userId)
+}
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some(user => user.userId == userId) &&
+        users.push({ userId, socketId })
+}
+
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId != socketId)
+}
+
+io.on("connection", (socket) => {
+
+    socket.on("addUser", userId => {
+        addUser(userId, socket.id)
+        io.emit("getUsers", users)
+    })
+
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId)
+        user?.socketId && io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text
+        })
+    })
+
+    socket.on("disconnect", () => {
+        console.log("a user is disconnected ")
+        removeUser(socket.id)
+        io.emit("getUsers", users)
+    })
 })
 
 app.use(cors());
